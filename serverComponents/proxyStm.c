7535f95f2cc25a8ce0d8f244d6e7a60c4d1f1b70
Martin Victory <mvictory@itba.edu.ar>
2018-06-02 22:30:53 -0300

    .h errors

--- serverComponents/proxyStm.c
+++ serverComponents/proxyStm.c
@@ -5,32 +5,20 @@
 
 const struct state_definition proxyStatesHandlers[] = {
     {
-        .state            = REQUEST,
-        /*.on_arrival       = hello_read_init,
-        .on_departure     = hello_read_close,
-        .on_read_ready    = hello_read*/
-		//.on_read_ready    = obtainOriginRead
+        .state = REQUEST,
 		.on_read_ready = requestRead, 
 		.on_write_ready = requestWrite,
 		.on_block_ready = requestBlockReady,
         .on_arrival = requestArrival,
         .on_departure = requestDeparture
     },{
-        .state            = RESPONSE,
+        .state = RESPONSE,
         .on_read_ready = responseRead,
         .on_write_ready = responseWrite
-        //.on_block_ready   = request_resolv_done,
-    },/*{
-        //.state            = CONNECT,
-        //.on_arrival       = request_connecting_init,
-        //.on_write_ready   = request_connecting,
     },{
-		.state            = ANSWER,
-		.on_write_ready   = writeEcho
-	},*/{
-        .state            = DONE,
+        .state = DONE,
     },{
-        .state            = ERROR,
+        .state = ERROR,
     }
 };
 
