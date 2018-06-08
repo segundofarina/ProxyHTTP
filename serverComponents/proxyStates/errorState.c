415aaed33982e91447d1d2f33c515a028dec2b0d
Florencia Cavallin <fcavallin@itba.edu.ar>
2018-06-08 18:32:08 -0300

    writeErrorToBuff

--- serverComponents/proxyStates/errorState.c
+++ serverComponents/proxyStates/errorState.c
@@ -37,9 +37,43 @@ unsigned errorWrite(struct selector_key * key) {
 }
 
 int writeErrorToBuff(char * ptr, const int count, enum error_code error) {
-    memcpy(ptr, "ERROR", 5);
-    ptr[6] = 0;
-    return 6;
+    char *ans = NULL;
+    switch(error){
+        case NO_ERROR:
+            return 0;
+        case BAD_REQUEST_400:
+            ans = "HTTP/1.1 400 Bad Request Error\r\nContent-Type: text/plain\r\nContent-Length: 17\r\n\r\n400 Bad Request\r\n\r\n";
+            break;
+        case LENGTH_REQUIRED_411:
+            ans = "HTTP/1.1 411 Length Required Error\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\n411 Length Required\r\n\r\n";
+            break;
+        case REQ_HEADER_TOO_LONG_431:
+            ans = "HTTP/1.1 431 Request Header Fields Too Large Error\r\nContent-Type: text/plain\r\nContent-Length: 37\r\n\r\n431 Request Header Fields Too Large\r\n\r\n";
+            break;
+        case INTERNAL_SERVER_ERR_500:
+            ans = "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\n500 Internal Server\r\n\r\n";
+            break;
+        case NOT_IMPLEMENTED_501:
+            ans = "HTTP/1.1 501 Internal Server Error\r\nContent-Type: text/plain\r\nContent-Length: 27\r\n\r\n501 Internal Server Error\r\n\r\n";
+            break;
+        case BAD_GATEWAY_502:
+            ans = "HTTP/1.1 502 Bad Gateway Error\r\nContent-Type: text/plain\r\nContent-Length: 17\r\n\r\n502 Bad Gateway\r\n\r\n";
+            break;
+        case HTTP_VER_ERR_505:
+            ans = "HTTP/1.1 505 HTTP Version Not Supported Error\r\nContent-Type: text/plain\r\nContent-Length: 32\r\n\r\n505 HTTP Version Not Supported\r\n\r\n";
+            break;
+        case LOOP_DETECTED_508:
+            ans = "HTTP/1.1 508 Loop Detected Error\r\nContent-Type: text/plain\r\nContent-Length: 19\r\n\r\n508 Loop Detected\r\n\r\n";
+            break;
+        default:
+            return -1;
+    }
+    int i = strlen(ans);
+    if(i > count){
+        return -1;
+    }
+    memcpy(ptr,ans, i);
+    return i;
 }
 
 unsigned setError(struct selector_key * key, enum error_code error) {
